52c0def10f2653fc167691278553ec79360d0ee6
Mathew Grabau <mgrabau@mathewgrabau.ca>
2019-01-26 01:48:54 -0600

    Building the controls for displaying the data for right now.

---
+++ NotesApp/Views/UserControls/NotebookControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotesApp.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Notebook.xaml
+    /// </summary>
+    public partial class NotebookControl : UserControl
+    {
+        /// <summary>
+        ///  The bound/displayed notebook associated to the control.
+        /// </summary>
+        public NotesApp.Models.Notebook Notebook
+        {
+            get { return (Models.Notebook)GetValue(NotebookProperty); }
+            set { SetValue(NotebookProperty, value); }
+        }
+
+        // Dependency property
+        public static readonly DependencyProperty NotebookProperty =
+            DependencyProperty.Register("Notebook", typeof(Models.Notebook), typeof(NotebookControl), new PropertyMetadata(null, SetValues));
+
+
+        public NotebookControl()
+        {
+            InitializeComponent();
+        }
+
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            NotebookControl notebookControl = d as NotebookControl;
+
+            if (notebookControl != null)
+            {
+                notebookControl.nameTextBlock.Text = (e.NewValue as Models.Notebook).Name;
+            }
+        }
+    }
+}
---
+++ NotesApp/Views/UserControls/NotesControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using NotesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotesApp.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for NotesControl.xaml
+    /// </summary>
+    public partial class NotesControl : UserControl
+    {
+        public Note Note
+        {
+            get
+            {
+                return (Note)GetValue(NoteProperty);
+            }
+            set
+            {
+                SetValue(NoteProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty NoteProperty = 
+            DependencyProperty.Register("Note", typeof(Note), typeof(NotesControl), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            NotesControl note = d as NotesControl;
+
+            if (note != null)
+            {
+                note.titleTextBlock.Text = (e.NewValue as Note).Title;
+                note.editedTextBlock.Text = (e.NewValue as Note).UpdatedTime.ToShortDateString();
+                note.contentTextBlock.Text = (e.NewValue as Note).Title;
+            }
+        }
+
+        public NotesControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
